2302fbd0d6c8f653d74af5f89b04ee5673050231
DaniilYuz <georgiferstor@gmail.com>
2025-12-24 19:45:01 +0100

    Remove game-expo folder

--- game-expo/app/(tabs)/index.tsx
+++
@@ -1,7 +0,0 @@
-// app/(tabs)/index.tsx
-import React from "react";
-import { GameScreen } from "../../src/screens/GameScreen"; // путь к твоему экрану
-
-export default function HomeScreen() {
-  return <GameScreen />;
-}
--- game-expo/app/(tabs)/options.tsx
+++
@@ -1,7 +0,0 @@
-// app/(tabs)/options.tsx
-import React from "react";
-import { OptionsScreen } from "@/src/screens/Options";
-
-export default function OptionsTab() {
-  return <OptionsScreen />;
-}
